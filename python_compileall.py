27d2f8c0e5a4909d5ca23bddc7a512a4efac6b5c
Meiyarasu <arasuu555@gmail.com>
2020-02-18 16:33:32 +0530

    print enable

--- python_compileall.py
+++ python_compileall.py
@@ -9,6 +9,8 @@ import compileall,shutil,os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 destination = dir_path+'\\destination'
 
+print(dir_path)
+
 if os.path.exists(destination):
     shutil.rmtree(destination)
 compileall.compile_dir(dir_path)
@@ -21,6 +23,7 @@ for root, subdirs, files in os.walk(destination):
             newname=os.path.join(root.replace('\__pycache__',''),file.split('.')[0] +'.pyc')
             shutil.move(oldname,newname)
         shutil.rmtree(root)
+print("Completed")
 
 
     
